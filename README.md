250fb3e42f4a18d8d147ab47634ffe62d505c970
akeril <185402721+akeril@users.noreply.github.com>
2024-10-17 22:46:27 +0530

    bootstrap.nix

---
+++ README.md
@@ -0,0 +1,21 @@
+# bootstrap.nix
+
+A simple bash script to help bootstrap NixOS installations. 
+
+NixOS is a Linux distribution where everything is described as code, with one exception: during installation, the disk partitioning and formatting are manual steps. **disko** aims to correct this sad omission.
+
+**bootstrap.nix** brings the NixOS installer and Disko together, enabling system installations with a single command.
+
+## Usage
+
+You need a flake which exposes both a `diskoConfiguration` and a `nixosConfiguration`. 
+
+Run the following command:
+
+```bash
+nix --experimental-features 'nix-command flakes' run github:kjabin/bootstrap.nix -- -f FLAKE_URI
+```
+
+Replace `FLAKE_URI` with the URI of your flake.
+
+
---
+++ flake.nix
@@ -0,0 +1,51 @@
+{
+  description = "A simple script to bootstrap nixos systems";
+
+  inputs = { nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable"; };
+
+  outputs = { self, nixpkgs }:
+  let
+    system = "x86_64-linux";
+    pkgs = nixpkgs.legacyPackages.${system};
+  in
+  {
+    defaultPackage.x86_64-linux = pkgs.writeShellScriptBin "bootstrap.nix" ''
+
+      showUsage() {
+        docs="Usage
+        * -f, --flake
+          fetch the nixos configuration flake from this uri
+        * -h, --help 
+          Show this help"
+        echo "$docs"
+      }
+
+      while [[ $# -gt 0 ]]; do
+        case "$1" in
+          -f | --flake)
+            flake=$2
+            shift
+            ;;
+          -h | --help)
+            showUsage
+            exit 0
+            ;;
+          *)
+            showUsage
+            exit 1
+            ;;
+        esac
+        shift
+      done
+
+      if [[ -z $flake ]]; then
+          echo "Missing flake uri"
+          exit 1
+      fi
+
+      nix --experimental-features 'nix-command flakes' run github:nix-community/disko -- --flake $flake -m disko
+      mount | grep /mnt
+      nixos-install --flake $flake --no-root-passwd
+    '';
+  };
+}
